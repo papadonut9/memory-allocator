da9434bd235715ecd136a5750f272cbc4e960e4a
dolor3s <papadonut@proton.me>
2022-12-07 08:52:03 +0530

    feat: malloc()

--- memalloc.c
+++ memalloc.c
@@ -20,14 +20,14 @@ union header
 		unsigned is_free;
 		// linked list implementation to keep track of non-contiguous memory blocks
 		struct header_t *next;
-	}s;
+	} s;
 	// aligning end of header block to actual memory block with a 16 byte alignment
 	ALIGN stub;
 };
 typedef union header header_t;
 
 // header and tail pointer to keep track of the list
-header_t  *head, *tail;
+header_t *head, *tail;
 
 // Thread lock mechanism to prevent two or more threads from conucrrently accessing memory
 // before any action acquire this lock and after you're done, just release this lock
@@ -35,14 +35,67 @@ pthread_mutex_t global_malloc_lock;
 
 // Check whether any memory blocks of given size are free in the linked list
 // first fit approach used
-header_t *get_free_block(size_t size){
+header_t *get_free_block(size_t size)
+{
 	header_t *curr = head;
 	while (curr)
 	{
-		if(curr->s.is_free && curr->s.size >= size){
+		// check if there is a free block which can accomodate requested size
+		if (curr->s.is_free && curr->s.size >= size)
+		{
 			return curr;
 			curr = curr->s.next;
 		}
 	}
 	return NULL;
+}
+
+// Allocating memory and returning pointer to the allocated memory
+void *malloc(size_t size)
+{
+	size_t total_size;
+	void *block;
+	header_t *header;
+
+	// if size is 0 then we return NULL
+	if (!size)
+		return NULL;
+
+	// acquiring lock for valid size
+	pthread_mutex_lock(&global_malloc_lock);
+	header = get_free_block(size);
+	if (header)
+	{
+		header->s.is_free = 0;
+		pthread_mutex_unlock(&global_malloc_lock);
+		// hiding headers and pointing to first byte of memory block
+		return (void *)(header + 1);
+	}
+	total_size = sizeof(header_t) + size;
+
+	// extending heap if no sufficiently large free block is found
+	block = sbrk(total_size);
+	if (block == (void *)-1)
+	{
+		pthread_mutex_unlock(&global_malloc_lock);
+		return NULL;
+	}
+
+	// Filling header with requested memory size
+	header = block;
+	header->s.size = size;
+	header->s.is_free = 0;
+	header->s.next = NULL;
+
+	if (!head)
+		head = header;
+	if (tail)
+		tail->s.next = header;
+	tail = header;
+
+	// releasing global lock
+	pthread_mutex_unlock(&global_malloc_lock);
+
+	// not exposing header to the caller
+	return (void *)(header + 1);
 }
